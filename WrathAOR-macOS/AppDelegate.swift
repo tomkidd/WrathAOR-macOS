bcd668c9f274ce939bc57c9ee96a575cd4942534
Tom Kidd <tomkidd@gmail.com>
2020-03-02 20:58:27 -0600

    Initial Commit

---
+++ WrathAOR-macOS/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  WrathAOR-macOS
+//
+//  Created by Tom Kidd on 3/2/20.
+//  Copyright © 2020 Tom Kidd. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
